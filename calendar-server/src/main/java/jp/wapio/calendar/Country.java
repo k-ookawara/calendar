2d4c5b9faba12a276a226aec8571ccbc61ef4b8e
okbabata <okawara@saunz.co.jp>
2024-08-07 01:08:38 +0900

    [clean] 変数名の修正

--- calendar-server/src/main/java/jp/wapio/calendar/Country.java
+++ calendar-server/src/main/java/jp/wapio/calendar/Country.java
@@ -13,13 +13,13 @@ public enum Country {
     TOKYO("Tokyo", 35.6785, 139.6823),
     NEW_YORK("New_York", 40.71, -74.01);
 
-    private String areaId;
-    private double latitude;
-    private double longitude;
+    private final String id;
+    private final double latitude;
+    private final double longitude;
 
-    public static Optional<Country> of(String areaId) {
+    public static Optional<Country> of(String id) {
         for (Country c : values()) {
-            if (c.areaId.equals(areaId)) {
+            if (c.id.equals(id)) {
                 return Optional.of(c);
             }
         }
